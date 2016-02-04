c8aed8d75f9d3f7e0dc0e9a26065d8cd060c814c
pbogucki <pbogucki@sfu.ca>
2016-02-04 06:03:48 -0800

    Refactors controller into multiple functions. View uses new scope variable so as to be able to access more stats.

--- dev/assets/scripts/controllers/MainController.js
+++ dev/assets/scripts/controllers/MainController.js
@@ -4,19 +4,76 @@ app.controller('MainController',
 	
 	$scope.title = "Teams";
 
+	var NYI = {
+		name : "NY Islanders",
+		fullName : "New York Islanders",
+		teamID : "NYI"
+	};
+
+	var NYR = {
+		name : "NY Rangers",
+		fullName : "New York Rangers",
+		teamID : "NYR"
+	};
+
 	function getTeams() {
 		teams.then(function(teamsData) {
 			$scope.teams = teamsData.data;
 			teamsDetailed.then(function(teamsDetailedData) {
-				$scope.teamsDetailed = teamsDetailedData;
-				teamsDetailedData.teams = teamsData.data;
-				for  (var item = 0; item <  teamsDetailedData.data.standings["info-teams"][0]["team-standing"].length; item++) {
-				}
+				var arrayTeamsWithStats = teamsDetailedData.data.standings["info-teams"][0]["team-standing"];
+				assignFullNameandIDToTeamsWithStats(arrayTeamsWithStats, teamsData.data);
+				$scope.teamsWithStats = arrayTeamsWithStats;
 			});
 		})
 		.catch(function(data) {
 			console.log("error");
 		});
+	}
+
+	function assignFullNameandIDToTeamsWithStats(arrayTeamsWithStats, teamsData) {
+		for  (var indexTeamWithStats = 0; indexTeamWithStats < arrayTeamsWithStats.length; indexTeamWithStats++) {
+			arrayTeamsWithStats[indexTeamWithStats].$.fullName = "FAIL";
+			for(var indexTeams = 0; indexTeams < teamsData.length; indexTeams++) {
+				if(teamsMatch(arrayTeamsWithStats[indexTeamWithStats], teamsData[indexTeams])) {
+					setFullNameAndID(arrayTeamsWithStats[indexTeamWithStats], teamsData[indexTeams]);
+					break;
+				}
+				else if (teamIsNYI(arrayTeamsWithStats[indexTeamWithStats])) {
+					setNYFullNameAndID(arrayTeamsWithStats[indexTeamWithStats], NYI);
+					break;
+				}
+				else if (teamIsNYR(arrayTeamsWithStats[indexTeamWithStats])) {
+					setNYFullNameAndID(arrayTeamsWithStats[indexTeamWithStats], NYR);
+					break;
+				}
+			}
+		}
+	}
+
+	function scanTeamsAndAssignFullNameandIDToTeamsWithStats(arrayTeamsWithStats, teamsData) {
+		
+	}
+
+	function teamsMatch(teamWithStats, team) {
+		return teamWithStats.$.name === team.name.substr(0, teamWithStats.$.name.length);
+	}
+
+	function teamIsNYI(teamWithStats) {
+		return teamWithStats.$.name === NYI.name;
+	}
+
+	function teamIsNYR(teamWithStats) {
+		return teamWithStats.$.name === NYR.name;
+	}
+
+	function setNYFullNameAndID(teamWithStats, team) {
+		teamWithStats.$.fullName = team.fullName;
+		teamWithStats.$.teamID = team.teamID;
+	}
+
+	function setFullNameAndID(teamwithStats, team) {
+		teamwithStats.$.fullName = team.name;
+		teamwithStats.$.teamID = team.teamID;
 	}
 
 	function getTeamsDetailed() {
